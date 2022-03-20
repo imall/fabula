602609d70698369c7456b5414ca2a4170477db7d
imall <imall841110@gmail.com>
2022-03-20 17:30:22 +0800

    'add'

---
+++ _fabula稿件20220320/js/home.js
@@ -0,0 +1,7 @@
+; (function () {
+let more_story = document.querySelector(".more-story")
+let more_story_button= document.querySelector(".more-story-button")
+more_story_button.addEventListener("click",()=>{
+    more_story.classList.add("click")
+})
+})()
